801420e34814f80e1750cd661d179e3c76c3cb76
Mariam181 <mariammora181@gmail.com>
2019-01-28 16:06:56 -0600

    'jquery'

--- Quiz2/script/main.js
+++ Quiz2/script/main.js
@@ -1,29 +1,12 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function() {
+    
+});
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("image");
-    let dots = document.getElementsByClassName("img");
-    let captionText = document.getElementById("title");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-}
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-}
+$(function(){
+    $('#slider div:gt(0)').hide();
+        setInterval(function(){
+            $('#slider div:first-child').fadeOut(1000)
+                .next('div').fadeIn(1000)
+                .end().appendTo('#slider');
+        },3000);
+});
